9a1beb529f6eae404ee5a5a9441bbfbf42d72124
Groni3000 <72792408+Groni3000@users.noreply.github.com>
2022-12-04 22:56:16 +0200

    made np sma version 2-d compatible

--- technical_indicators/indicators.py
+++ technical_indicators/indicators.py
@@ -8,17 +8,21 @@ class SimpleMovingAverage(Indicator):
 
     def __init__(self, window: int = 2):
         self._window = window
-        self._nans = np.full(self._window - 1, np.NaN)
 
     def run(self, data: pd.Series | pd.DataFrame):
         return data.rolling(self._window).mean()
 
     def np_run(self, data: np.ndarray):
         """~6x times faster than pure pandas `run` method"""
-        cumsum_vec = np.cumsum(np.insert(data, 0, 0))
+        cumsum_vec = np.cumsum(np.insert(data, 0, 0, axis=0), axis=0)
         np_sma = (
             cumsum_vec[self._window :] - cumsum_vec[: -self._window]
         ) / self._window
+
+        if len(data.shape) == 1:
+            self._nans = np.full(self._window - 1, np.NaN)
+        else:
+            self._nans = np.full((self._window - 1, data.shape[1]), np.NaN)
 
         return np.concatenate(
             [self._nans, np_sma]
